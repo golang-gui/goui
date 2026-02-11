6ba32a135309a255e4403d0ee99d374219d0b027
xuges <xuges@qq.com>
2026-02-11 15:43:53 +0800

    Add event type.

---
+++ platform/events/event.go
@@ -0,0 +1,38 @@
+package events
+
+type Event interface {
+	Type() EventType
+	Accept()
+	Ignore()
+	Accepted() (b bool)
+	SetAccepted(b bool)
+}
+
+type EventType int
+
+const (
+	Native EventType = iota
+	Close
+)
+
+type EventHandler func(event Event)
+
+type EventBase struct {
+	accepted bool
+}
+
+func (e *EventBase) Accept() {
+	e.accepted = true
+}
+
+func (e *EventBase) Ignore() {
+	e.accepted = false
+}
+
+func (e *EventBase) Accepted() bool {
+	return e.accepted
+}
+
+func (e *EventBase) SetAccepted(v bool) {
+	e.accepted = v
+}
