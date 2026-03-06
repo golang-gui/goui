b1ea272d148c141d25aebb74f907d8ca2f213ca3
xuges <xuges@qq.com>
2026-03-06 09:59:15 +0800

    Add opengl binding.

---
+++ platform/gpu/opengl/gl/api.go
@@ -0,0 +1,286 @@
+package gl
+
+import (
+	"runtime"
+	"unsafe"
+
+	"github.com/golang-gui/goui/core/cgo"
+)
+
+type (
+	LoadFunc func(symbol string) (fn uintptr)
+	CallFunc func(fn uintptr, args ...uintptr) uintptr
+)
+
+func Init(loadFn LoadFunc, callFn CallFunc) (err error) {
+	return loadGlFuncs(loadFn, callFn)
+}
+
+func GetError() Enum {
+	ret := call(glGetError)
+	return Enum(ret)
+}
+
+func BindTexture(target Enum, texture Uint) {
+	call(glBindTexture, uintptr(target), uintptr(texture))
+}
+
+func DeleteTextures(textures []Uint) {
+	//glDeleteTextures(GLsizei n, const GLuint *textures)
+	call(glDeleteTextures, uintptr(len(textures)), uintptr(cgo.CSlice(textures)))
+}
+
+func GenTextures(n Sizei) (textures []Uint) {
+	//glGenTextures(GLsizei n, GLuint *textures)
+	textures = make([]Uint, n)
+	call(glGenTextures, uintptr(len(textures)), uintptr(cgo.CSlice(textures)))
+	runtime.KeepAlive(textures)
+	return
+}
+
+func ActiveTexture(texture Enum) {
+	call(glActiveTexture, uintptr(texture))
+}
+
+func TexImage2D(target Enum, level Int, internalFormat Int, width, height Sizei, border Int, format, typ Enum, pixels unsafe.Pointer) {
+	call(glTexImage2D, uintptr(target), uintptr(level), uintptr(internalFormat), uintptr(width), uintptr(height), uintptr(border), uintptr(format), uintptr(typ), uintptr(pixels))
+}
+
+func TexSubImage2D(target Enum, level, xOffset, yOffset Int, width, height Sizei, format, typ Enum, pixels unsafe.Pointer) {
+	call(glTexSubImage2D, uintptr(target), uintptr(level), uintptr(xOffset), uintptr(yOffset), uintptr(width), uintptr(height), uintptr(format), uintptr(typ), uintptr(pixels))
+}
+
+func TexParameteri(target Enum, name Enum, param Int) {
+	call(glTexParameteri, uintptr(target), uintptr(name), uintptr(param))
+}
+
+func BlendFuncSeparate(sFactorRgb, dFactorRgb, sFactorAlpha, dFactorAlpha Enum) {
+	call(glBlendFuncSeparate, uintptr(sFactorRgb), uintptr(dFactorRgb), uintptr(sFactorAlpha), uintptr(dFactorAlpha))
+}
+
+func CreateProgram() Uint {
+	ret := call(glCreateProgram)
+	return Uint(ret)
+}
+
+func DeleteProgram(program Uint) {
+	call(glDeleteProgram, uintptr(program))
+}
+
+func GetProgramiv(program Uint, name Enum) (value Int) {
+	//glGetProgramiv(GLuint program, GLenum pname, GLint *params)
+	call(glGetProgramiv, uintptr(program), uintptr(name), uintptr(unsafe.Pointer(&value)))
+	return
+}
+
+func GetProgramInfoLog(program Uint) string {
+	//glGetProgramInfoLog(GLuint program, GLsizei bufSize, GLsizei *length, GLchar *infoLog)
+	length := GetProgramiv(program, GL_INFO_LOG_LENGTH)
+	if length != 0 {
+		buf := make([]byte, length)
+		call(glGetProgramInfoLog, uintptr(program), uintptr(length), uintptr(unsafe.Pointer(&length)), uintptr(unsafe.Pointer(cgo.CSlice(buf))))
+		return cgo.GoStringNTemp(cgo.CSlice(buf), int(length))
+		runtime.KeepAlive(buf)
+	}
+	return ""
+}
+
+func AttachShader(program, shader Uint) {
+	call(glAttachShader, uintptr(program), uintptr(shader))
+}
+
+func BindAttribLocation(program Uint, index Uint, name string) {
+	//glBindAttribLocation(GLuint program, GLuint index, const GLchar *name)
+	cName := cgo.CString(name)
+	call(glBindAttribLocation, uintptr(program), uintptr(index), uintptr(cName))
+	runtime.KeepAlive(cName)
+}
+
+func LinkProgram(program Uint) {
+	call(glLinkProgram, uintptr(program))
+}
+
+func UseProgram(program Uint) {
+	call(glUseProgram, uintptr(program))
+}
+
+func GetUniformLocation(program Uint, name string) Int {
+	//glGetUniformLocation(GLuint program, const GLchar *name)
+	cName := cgo.CString(name)
+	ret := call(glGetUniformLocation, uintptr(program), uintptr(cName))
+	runtime.KeepAlive(cName)
+	return Int(ret)
+}
+
+func CreateShader(shaderType Enum) Uint {
+	ret := call(glCreateShader, uintptr(shaderType))
+	return Uint(ret)
+}
+
+func DeleteShader(shader Uint) {
+	call(glDeleteShader, uintptr(shader))
+}
+
+func GetShaderInfoLog(shader Uint) string {
+	//glGetShaderInfoLog)(GLuint shader, GLsizei bufSize, GLsizei *length, GLchar *infoLog)
+	length := GetShaderiv(shader, GL_INFO_LOG_LENGTH)
+	if length != 0 {
+		buf := make([]byte, length)
+		call(glGetShaderInfoLog, uintptr(shader), uintptr(length), 0, uintptr(unsafe.Pointer(cgo.CSlice(buf))))
+		return cgo.GoStringNTemp(cgo.CSlice(buf), int(length))
+		runtime.KeepAlive(buf)
+	}
+	return ""
+}
+
+func ShaderSource(shader Uint, sources []string) {
+	//glShaderSource(GLuint shader, GLsizei count, const GLchar *const*string, const GLint *length)
+	cSources := make([]unsafe.Pointer, len(sources))
+	for i := range sources {
+		src := cgo.CString(sources[i])
+		defer runtime.KeepAlive(src)
+		cSources[i] = src
+	}
+	call(glShaderSource, uintptr(shader), uintptr(len(sources)), uintptr(cgo.CSlice(cSources)), 0)
+	runtime.KeepAlive(cSources)
+}
+
+func CompileShader(shader Uint) {
+	call(glCompileShader, uintptr(shader))
+}
+
+func GetShaderiv(shader Uint, name Enum) (value Int) {
+	//glGetShaderiv(GLuint shader, GLenum pname, GLint *params)
+	call(glGetShaderiv, uintptr(shader), uintptr(name), uintptr(unsafe.Pointer(&value)))
+	return
+}
+
+func GenVertexArrays(n Sizei) (arrays []Uint) {
+	//glGenVertexArrays(GLsizei n, GLuint *arrays)
+	arrays = make([]Uint, n)
+	call(glGenVertexArrays, uintptr(len(arrays)), uintptr(cgo.CSlice(arrays)))
+	runtime.KeepAlive(arrays)
+	return
+}
+
+func BindVertexArray(array Uint) {
+	call(glBindVertexArray, uintptr(array))
+}
+
+func GenBuffers(n Sizei) (buffers []Uint) {
+	//glGenBuffers(GLsizei n, GLuint *buffers)
+	buffers = make([]Uint, n)
+	call(glGenBuffers, uintptr(len(buffers)), uintptr(cgo.CSlice(buffers)))
+	runtime.KeepAlive(buffers)
+	return
+}
+
+func BindBuffer(target Enum, buffer Uint) {
+	call(glBindBuffer, uintptr(target), uintptr(buffer))
+}
+
+func DeleteBuffers(buffers []Uint) {
+	//glDeleteBuffers(GLsizei n, const GLuint *buffers)
+	call(glDeleteBuffers, uintptr(len(buffers)), uintptr(cgo.CSlice(buffers)))
+	runtime.KeepAlive(buffers)
+}
+
+func BufferData[T any](target Enum, data []T, usage Enum) {
+	//void glBufferData(GLenum target, GLsizeiptr size, const void *data, GLenum usage)
+	var zero T
+	call(glBufferData, uintptr(target), uintptr(len(data))*unsafe.Sizeof(zero), uintptr(cgo.CSlice(data)), uintptr(usage))
+	runtime.KeepAlive(data)
+}
+
+func EnableVertexAttribArray(index Uint) {
+	call(glEnableVertexAttribArray, uintptr(index))
+}
+
+func DisableVertexAttribArray(index Uint) {
+	call(glDisableVertexAttribArray, uintptr(index))
+}
+
+func VertexAttribPointer(index Uint, size Int, typ Enum, normalized bool, stride Sizei, pointer uintptr) {
+	call(glVertexAttribPointer, uintptr(index), uintptr(size), uintptr(typ), uintptr(cgo.CBool(normalized)), uintptr(stride), pointer)
+}
+
+func DeleteVertexArrays(arrays []Uint) {
+	//glDeleteVertexArrays(GLsizei n, const GLuint *arrays)
+	call(glDeleteVertexArrays, uintptr(len(arrays)), uintptr(cgo.CSlice(arrays)))
+}
+
+func PixelStorei(name Enum, param Int) {
+	call(glPixelStorei, uintptr(name), uintptr(param))
+}
+
+func GenerateMipmap(target Enum) {
+	call(glGenerateMipmap, uintptr(target))
+}
+
+func Uniform1i(location, v1 Int) {
+	call(glUniform1i, uintptr(location), uintptr(v1))
+}
+
+func Uniform2fv(location Int, values []Float) {
+	//glUniform2fv(GLint location, GLsizei count, const GLfloat *value)
+	call(glUniform2fv, uintptr(location), uintptr(len(values)), uintptr(cgo.CSlice(values)))
+	runtime.KeepAlive(values)
+}
+
+func Uniform4fv(location Int, values []Float) {
+	//glUniform4fv(GLint location, GLsizei count, const GLfloat *value)
+	call(glUniform4fv, uintptr(location), uintptr(len(values)), uintptr(cgo.CSlice(values)))
+	runtime.KeepAlive(values)
+}
+
+func Enable(cap Enum) {
+	call(glEnable, uintptr(cap))
+}
+
+func Disable(cap Enum) {
+	call(glDisable, uintptr(cap))
+}
+
+func ColorMask(red, green, blue, alpha bool) {
+	call(glColorMask, uintptr(cgo.CBool(red)), uintptr(cgo.CBool(green)), uintptr(cgo.CBool(blue)), uintptr(cgo.CBool(alpha)))
+}
+
+func StencilMask(mask Uint) {
+	call(glStencilMask, uintptr(mask))
+}
+
+func StencilFunc(fn Enum, ref Int, mask Uint) {
+	call(glStencilFunc, uintptr(fn), uintptr(ref), uintptr(mask))
+}
+
+func StencilOp(fail, zFail, zPass Enum) {
+	call(glStencilOp, uintptr(fail), uintptr(zFail), uintptr(zPass))
+}
+
+func StencilOpSeparate(face, sFail, dpFail, dpPass Enum) {
+	call(glStencilOpSeparate, uintptr(face), uintptr(sFail), uintptr(dpFail), uintptr(dpPass))
+}
+
+func DrawArrays(mode Enum, first Int, count Sizei) {
+	call(glDrawArrays, uintptr(mode), uintptr(first), uintptr(count))
+}
+
+func CullFace(mode Enum) {
+	call(glCullFace, uintptr(mode))
+}
+
+func FrontFace(mode Enum) {
+	call(glFrontFace, uintptr(mode))
+}
+
+func Finish() {
+	call(glFinish)
+}
+
+func Viewport(x, y Int, width, height Sizei) {
+	call(glViewport, uintptr(x), uintptr(y), uintptr(width), uintptr(height))
+}
+
+func Clear(mask Bitfield) {
+	call(glClear, uintptr(mask))
+}
---
+++ platform/gpu/opengl/gl/load.go
@@ -0,0 +1,283 @@
+package gl
+
+import "fmt"
+
+var (
+	load LoadFunc
+	call CallFunc
+
+	glGetError                 uintptr
+	glBindTexture              uintptr
+	glDeleteTextures           uintptr
+	glGenTextures              uintptr
+	glActiveTexture            uintptr
+	glTexImage2D               uintptr
+	glTexSubImage2D            uintptr
+	glTexParameteri            uintptr
+	glBlendFuncSeparate        uintptr
+	glCreateProgram            uintptr
+	glDeleteProgram            uintptr
+	glGetProgramiv             uintptr
+	glGetProgramInfoLog        uintptr
+	glAttachShader             uintptr
+	glBindAttribLocation       uintptr
+	glLinkProgram              uintptr
+	glUseProgram               uintptr
+	glGetUniformLocation       uintptr
+	glCreateShader             uintptr
+	glDeleteShader             uintptr
+	glGetShaderInfoLog         uintptr
+	glShaderSource             uintptr
+	glCompileShader            uintptr
+	glGetShaderiv              uintptr
+	glGenVertexArrays          uintptr
+	glBindVertexArray          uintptr
+	glGenBuffers               uintptr
+	glBindBuffer               uintptr
+	glDeleteBuffers            uintptr
+	glBufferData               uintptr
+	glEnableVertexAttribArray  uintptr
+	glDisableVertexAttribArray uintptr
+	glVertexAttribPointer      uintptr
+	glDeleteVertexArrays       uintptr
+	glPixelStorei              uintptr
+	glGenerateMipmap           uintptr
+	glUniform1i                uintptr
+	glUniform2fv               uintptr
+	glUniform4fv               uintptr
+	glEnable                   uintptr
+	glDisable                  uintptr
+	glColorMask                uintptr
+	glStencilMask              uintptr
+	glStencilFunc              uintptr
+	glStencilOp                uintptr
+	glStencilOpSeparate        uintptr
+	glDrawArrays               uintptr
+	glCullFace                 uintptr
+	glFrontFace                uintptr
+	glFinish                   uintptr
+	glViewport                 uintptr
+	glClear                    uintptr
+)
+
+func loadGlFuncs(loadFn LoadFunc, callFn CallFunc) (err error) {
+	load, call = loadFn, callFn
+	glGetError, err = loadGlFunc("glGetError")
+	if err != nil {
+		return
+	}
+	glBindTexture, err = loadGlFunc("glBindTexture")
+	if err != nil {
+		return
+	}
+	glDeleteTextures, err = loadGlFunc("glDeleteTextures")
+	if err != nil {
+		return
+	}
+	glGenTextures, err = loadGlFunc("glGenTextures")
+	if err != nil {
+		return
+	}
+	glActiveTexture, err = loadGlFunc("glActiveTexture")
+	if err != nil {
+		return
+	}
+	glTexImage2D, err = loadGlFunc("glTexImage2D")
+	if err != nil {
+		return
+	}
+	glTexSubImage2D, err = loadGlFunc("glTexSubImage2D")
+	if err != nil {
+		return
+	}
+	glTexParameteri, err = loadGlFunc("glTexParameteri")
+	if err != nil {
+		return
+	}
+	glBlendFuncSeparate, err = loadGlFunc("glBlendFuncSeparate")
+	if err != nil {
+		return
+	}
+	glCreateProgram, err = loadGlFunc("glCreateProgram")
+	if err != nil {
+		return
+	}
+	glDeleteProgram, err = loadGlFunc("glDeleteProgram")
+	if err != nil {
+		return
+	}
+	glGetProgramiv, err = loadGlFunc("glGetProgramiv")
+	if err != nil {
+		return
+	}
+	glGetProgramInfoLog, err = loadGlFunc("glGetProgramInfoLog")
+	if err != nil {
+		return
+	}
+	glAttachShader, err = loadGlFunc("glAttachShader")
+	if err != nil {
+		return
+	}
+	glBindAttribLocation, err = loadGlFunc("glBindAttribLocation")
+	if err != nil {
+		return
+	}
+	glLinkProgram, err = loadGlFunc("glLinkProgram")
+	if err != nil {
+		return
+	}
+	glUseProgram, err = loadGlFunc("glUseProgram")
+	if err != nil {
+		return
+	}
+	glGetUniformLocation, err = loadGlFunc("glGetUniformLocation")
+	if err != nil {
+		return
+	}
+	glCreateShader, err = loadGlFunc("glCreateShader")
+	if err != nil {
+		return
+	}
+	glDeleteShader, err = loadGlFunc("glDeleteShader")
+	if err != nil {
+		return
+	}
+	glGetShaderInfoLog, err = loadGlFunc("glGetShaderInfoLog")
+	if err != nil {
+		return
+	}
+	glShaderSource, err = loadGlFunc("glShaderSource")
+	if err != nil {
+		return
+	}
+	glCompileShader, err = loadGlFunc("glCompileShader")
+	if err != nil {
+		return
+	}
+	glGetShaderiv, err = loadGlFunc("glGetShaderiv")
+	if err != nil {
+		return
+	}
+	glGenVertexArrays, err = loadGlFunc("glGenVertexArrays")
+	if err != nil {
+		return
+	}
+	glBindVertexArray, err = loadGlFunc("glBindVertexArray")
+	if err != nil {
+		return
+	}
+	glGenBuffers, err = loadGlFunc("glGenBuffers")
+	if err != nil {
+		return
+	}
+	glBindBuffer, err = loadGlFunc("glBindBuffer")
+	if err != nil {
+		return
+	}
+	glDeleteBuffers, err = loadGlFunc("glDeleteBuffers")
+	if err != nil {
+		return
+	}
+	glBufferData, err = loadGlFunc("glBufferData")
+	if err != nil {
+		return
+	}
+	glEnableVertexAttribArray, err = loadGlFunc("glEnableVertexAttribArray")
+	if err != nil {
+		return
+	}
+	glDisableVertexAttribArray, err = loadGlFunc("glDisableVertexAttribArray")
+	if err != nil {
+		return
+	}
+	glVertexAttribPointer, err = loadGlFunc("glVertexAttribPointer")
+	if err != nil {
+		return
+	}
+	glDeleteVertexArrays, err = loadGlFunc("glDeleteVertexArrays")
+	if err != nil {
+		return
+	}
+	glPixelStorei, err = loadGlFunc("glPixelStorei")
+	if err != nil {
+		return
+	}
+	glGenerateMipmap, err = loadGlFunc("glGenerateMipmap")
+	if err != nil {
+		return
+	}
+	glUniform1i, err = loadGlFunc("glUniform1i")
+	if err != nil {
+		return
+	}
+	glUniform2fv, err = loadGlFunc("glUniform2fv")
+	if err != nil {
+		return
+	}
+	glUniform4fv, err = loadGlFunc("glUniform4fv")
+	if err != nil {
+		return
+	}
+	glEnable, err = loadGlFunc("glEnable")
+	if err != nil {
+		return
+	}
+	glDisable, err = loadGlFunc("glDisable")
+	if err != nil {
+		return
+	}
+	glColorMask, err = loadGlFunc("glColorMask")
+	if err != nil {
+		return
+	}
+	glStencilMask, err = loadGlFunc("glStencilMask")
+	if err != nil {
+		return
+	}
+	glStencilFunc, err = loadGlFunc("glStencilFunc")
+	if err != nil {
+		return
+	}
+	glStencilOp, err = loadGlFunc("glStencilOp")
+	if err != nil {
+		return
+	}
+	glStencilOpSeparate, err = loadGlFunc("glStencilOpSeparate")
+	if err != nil {
+		return
+	}
+	glDrawArrays, err = loadGlFunc("glDrawArrays")
+	if err != nil {
+		return
+	}
+	glCullFace, err = loadGlFunc("glCullFace")
+	if err != nil {
+		return
+	}
+	glFrontFace, err = loadGlFunc("glFrontFace")
+	if err != nil {
+		return
+	}
+	glFinish, err = loadGlFunc("glFinish")
+	if err != nil {
+		return
+	}
+	glViewport, err = loadGlFunc("glViewport")
+	if err != nil {
+		return
+	}
+	glClear, err = loadGlFunc("glClear")
+	if err != nil {
+		return
+	}
+
+	return nil
+}
+
+func loadGlFunc(name string) (fn uintptr, err error) {
+	fn = load(name)
+	if fn == 0 {
+		err = fmt.Errorf("gl: can not load %s", name)
+	}
+	return
+}
