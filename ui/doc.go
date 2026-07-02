ae653915b83a14cf913aba5ecde5dab3c18eeaf3
xuges <xuges@qq.com>
2026-07-02 17:25:17 +0800

    Add package doc.

---
+++ ui/doc.go
@@ -0,0 +1,6 @@
+// Package ui implements GOUI's declarative view layer.
+//
+// It is a thin adapter over package gui. Application code describes windows and
+// widgets with ordinary Go values and chainable methods, then ui reconciles
+// those values into gui windows and widgets.
+package ui
